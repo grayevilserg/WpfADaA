e0f147e33f45779e3512622288a59362353c3065
grayevilserg <grayevilserg@mail.ru>
2018-08-14 16:35:51 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Classes/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Classes
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        /*public string Email { get; set; }
+        public string CreationDate { get; set; }
+        public string AccountStatus { get; set; }*/
+
+    }
+}
